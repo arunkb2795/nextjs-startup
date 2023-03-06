5cd96508827f70c5b5244594d966315cccb29b50
arunkb2795 <arunkb27@gmail.com>
2023-03-06 19:19:09 +0530

    more attributes

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,10 +27,28 @@ export async function getStaticProps() {
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData.toString());
+
+  console.log(data.products.length)
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/no-data'
+      }
+    }
+  }
+
+  if (data.products.length === 0) {
+    return {
+      notFound: true
+    }
+  }
+
+  
   return {
     props: {
       posts: data.products
     },
-    revalidate: 10
+    revalidate: 10,
   }
 }
